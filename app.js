f4b2a3e831793fc2c2fec39870840664dbbb5f7c
developer\Antonio <ciceroapps@gmail.com>
2017-10-24 22:55:02 -0400

    MainFunctionality - loads website into iframe from querystring acicero

---
+++ app.js
@@ -0,0 +1,29 @@
+var getQueryParam = function(name, url) {
+	if (!url) url = window.location.href;
+	name = name.replace(/[\[\]]/g, "\\$&");
+	var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
+	var results = regex.exec(url);
+	if (!results) return null;
+	if (!results[2]) return '';
+	return decodeURIComponent(results[2].replace(/\+/g, " "));
+};
+
+var getData = function (data) {
+	if (data && data.query && data.query.results && data.query.results.resources && data.query.results.resources.content && data.query.results.resources.status == 200) loadHTML(data.query.results.resources.content);
+	else if (data && data.error && data.error.description) loadHTML(data.error.description);
+	else loadHTML('Error: Cannot load ' + url);
+};
+
+var loadURL = function (src) {
+	url = src;
+	var script = document.createElement('script');
+	script.src = 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20data.headers%20where%20url%3D%22' + encodeURIComponent(url) + '%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=getData';
+	document.body.appendChild(script);
+};
+
+var loadHTML = function (html) {
+	iframe.src = 'about:blank';
+	iframe.contentWindow.document.open();
+	iframe.contentWindow.document.write(html.replace(/<head>/i, '<head><base href="' + url + '"><scr' + 'ipt>document.addEventListener("click", function(e) { if(e.target && e.target.nodeName == "A") { e.preventDefault(); parent.loadURL(e.target.href); } });</scr' + 'ipt>'));
+	iframe.contentWindow.document.close();
+}
